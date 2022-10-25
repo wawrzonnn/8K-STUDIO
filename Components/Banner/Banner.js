89dcc7e126e70eec0c0dae74731845e85dae23a1
wawrzonnn <norbert.wawrzonek@gmail.com>
2022-10-25 23:58:14 +0200

    fixed menu display in mobile view

--- Components/Banner/Banner.js
+++ Components/Banner/Banner.js
@@ -6,8 +6,8 @@ const Banner = () => {
 		<>
 		
 		<div className='banner__box'>
-		<video className='test2' src={"../../public/video.mp4"} autoPlay loop muted/>  
-				<div className='test'>
+		<video className='banner__video' src={"../../public/video.mp4"} autoPlay loop muted/>  
+				<div className='banner__text--box'>
 					<h1 className='banner__text1'>8K STUDIO</h1>
 					<h2 className='banner__text2'>PRODUKCJA FILMOWA I TELEWIZYJNA</h2></div>
 					<img src='../../public/produkcja-filmow-jelenia-gora.jpg'/>
